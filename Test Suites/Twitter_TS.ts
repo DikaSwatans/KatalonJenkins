a704e60d8a5b508b7c37f7705e4f13f3d7392fa1
Swatans <Swatans@DESKTOP-NARHO9B>
2021-01-31 21:19:01 +0700

    added twitter_ts

---
+++ Test Suites/Twitter_TS.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Twitter_TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>78dffb39-a2f5-4e89-9d7e-d61b72959345</testSuiteGuid>
+</TestSuiteEntity>
